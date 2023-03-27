a74707a26a0658fb51c160af7023f9da47d47b09
nautilo <8lapse@gmail.com>
2023-03-27 04:35:50 -0300

    avance estilos responsivos

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+document.addEventListener("mousemove", parallax);
+
+function parallax(event) {
+    const x = -event.pageX/100;
+    const y = -event.pageY/100;
+    document.getElementById("info-perfil-productora").style.transform = `translateX(${-x}px) translateY(${y}px)`;
+    document.getElementById("info-perfil-artista").style.transform = `translateX(${x}px) translateY(${y}px)`;  
+}
