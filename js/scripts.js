8b825861cb408d12fc9eb40bdd4c835d4b75b1d2
nautilo <8lapse@gmail.com>
2023-03-27 15:35:57 -0300

    ventana flotante registro

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,5 @@ function parallax(event) {
     document.getElementById("info-perfil-productora").style.transform = `translateX(${-x}px) translateY(${y}px)`;
     document.getElementById("info-perfil-artista").style.transform = `translateX(${x}px) translateY(${y}px)`;  
 }
+
+jQuery.noConflict();jQuery(document).ready(function(){jQuery('#spoiler1').click(function(){jQuery('.mostrar1').slideToggle("slow");});});
