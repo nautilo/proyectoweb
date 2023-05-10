9c115c0da399cc46c9e1cd199f77aebb7bb34d96
nautilo <8lapse@gmail.com>
2023-05-09 22:32:03 -0400

    Validador y calculadora de total de la entrada
    Co-Authored-By: carlospitau <128086076+carlospitau@users.noreply.github.com>

--- js/validar_cantidad.js
+++ js/validar_cantidad.js
@@ -1,3 +1,26 @@
+const precioUnitario = 3000;
+const precio2Por1 = 5000;
+document.getElementById("cantidadentradas").addEventListener("change",calcularTotal)
+
+function calcularTotal(event){
+    var total = 0;
+    var cantidadEntradas = document.getElementById("cantidadentradas").value;
+    var subtotal = (cantidadEntradas * precioUnitario);
+    if(cantidadEntradas%2==0){
+        total=precio2Por1*(cantidadEntradas/2);
+    }else{
+        total=5000*(cantidadEntradas-1)/2 + 3000;
+    }
+    var descuento = subtotal-total;
+    document.getElementById("subtotal").innerText = "$"+subtotal.toString();
+    document.getElementById("total").innerText = "$"+total.toString();
+    if (descuento==0){
+        document.getElementById("descuento").innerText = "$"+descuento.toString();
+    }else{
+        document.getElementById("descuento").innerText = "-$"+descuento.toString();
+    }
+}
+
 $(document).ready(function () {
     $.validator.addMethod("positive_integer", function (value, element) {
       // Sólo permite números enteros positivos mayores que 0
