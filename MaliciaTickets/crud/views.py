37fa18a43de9bf861db56efc508716b90853165a
nautilo <8lapse@gmail.com>
2023-06-12 17:03:05 -0400

    Creadas las vistas del CRUD
    ...
    
    Co-Authored-By: carlospitau <128086076+carlospitau@users.noreply.github.com>

--- MaliciaTickets/crud/views.py
+++ MaliciaTickets/crud/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Evento
+from .forms import EventoForm, CustomUserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required, permission_required
 
-# Create your views here.
+def lista_eventos(request):
+    eventos = Evento.objects.all()
+    return render(request, 'crud/lista_eventos.html', {'eventos': eventos})
+
+
+def crear_evento(request):
+    if request.method == 'POST':
+        form = EventoForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "¡Tu evento ya está publicado!")
+            return redirect('lista_eventos')
+    else:
+        form = EventoForm()
+        return render(request, 'crud/crear_evento.html', {'form': form})
+
+
+def editar_evento(request, id):
+    evento = get_object_or_404(Evento, id=id)
+    if request.method == 'POST':
+        form = EventoForm(request.POST, request.FILES, instance=evento)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Evento modificado correctamente.")
+            return redirect('lista_eventos')
+    else:
+        form = EventoForm(instance=evento)
+    return render(request, 'crud/editar_evento.html', {'form': form, 'evento': evento})
+
+
+def eliminar_evento(request, id):
+    evento = get_object_or_404(Evento, id=id)
+    if request.method == 'POST':
+        evento.delete()
+        messages.success(request, "Evento eliminado correctamente.")
+        return redirect('lista_eventos')
+    return render(request, 'crud/eliminar_evento.html', {'evento': evento})
