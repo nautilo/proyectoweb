899bb5007b4d428f924d098483496d0e6b87f18f
nautilo <8lapse@gmail.com>
2023-06-12 17:22:26 -0400

    mapeadores y plantillas crud
    Co-Authored-By: carlospitau <128086076+carlospitau@users.noreply.github.com>

--- MaliciaTickets/crud/urls.py
+++ MaliciaTickets/crud/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns=[
     path('',views.lista_eventos,name='lista_eventos'),
-    path('crear/',views.crear_evento,name='crear_evento'),
-    path('editar/<str:id>/',views.editar_evento,name='editar_evento'),
-    path('eliminar/<str:id>/',views.eliminar_evento,name='eliminar_evento'),
+    path('crear_evento/',views.crear_evento,name='crear_evento'),
+    path('editar_evento/<str:id>/',views.editar_evento,name='editar_evento'),
+    path('eliminar_evento/<str:id>/',views.eliminar_evento,name='eliminar_evento'),
 ]
