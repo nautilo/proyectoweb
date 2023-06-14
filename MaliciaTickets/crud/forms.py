f6e580691218b3f453496f3b59abe416c3e918b6
nautilo <8lapse@gmail.com>
2023-06-14 16:06:35 -0400

    Crud Eventos y plantillas extendidas
    Co-Authored-By: carlospitau <128086076+carlospitau@users.noreply.github.com>

--- MaliciaTickets/crud/forms.py
+++ MaliciaTickets/crud/forms.py
@@ -7,9 +7,16 @@ from django.contrib.auth.models import User
 
 
 class EventoForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(EventoForm, self).__init__(*args, **kwargs)
+        self.fields['imagen'].required = False
     class Meta:
         model = Evento
         fields = '__all__'
+        widgets = {
+            'fecha': forms.DateInput(attrs={'type': 'date'}),
+            'hora': forms.TimeInput(format='%H:%M', attrs={'type': 'time'})
+        }
 
 class CustomUserCreationForm(UserCreationForm):
     tipo_usuario = forms.CharField(max_length=1)
--- MaliciaTickets/crud/urls.py
+++ MaliciaTickets/crud/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns=[
     path('',views.lista_eventos,name='lista_eventos'),
     path('crear_evento/',views.crear_evento,name='crear_evento'),
-    path('editar_evento/<str:id>/',views.editar_evento,name='editar_evento'),
-    path('eliminar_evento/<str:id>/',views.eliminar_evento,name='eliminar_evento'),
+    path('editar_evento/<int:id>/',views.editar_evento,name='editar_evento'),
+    path('eliminar_evento/<int:id>/',views.eliminar_evento,name='eliminar_evento'),
 ]
